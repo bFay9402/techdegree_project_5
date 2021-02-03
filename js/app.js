e5516f95d12c71aa977f53ec56a12e929275da26
bFay9402 <bfay9402@gmail.com>
2021-02-03 13:24:35 -0500

    finished project - added js functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 baguetteBox.run('.gallery');
+baguetteBox.run('.gallery', {
+  captions: true, 
+  buttons: 'auto',
+  animation: 'slideIn', 
+  verlayBackgroundColor: 'rgba(0,0,0,.8)'
+});
+const search = new Filter('search', 'data-caption');
